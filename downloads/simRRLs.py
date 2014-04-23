d130568a579d61d40e0dd521ae6242d3f89cec1b
daeaton <daeaton.chicago@gmail.com>
2014-04-23 00:23:37 -0400

    dropouts line added

--- downloads/simRRLs.py
+++ downloads/simRRLs.py
@@ -3,7 +3,7 @@
 ##  author:  Deren Eaton                        ##
 ##  contact: deren.eaton@yale.edu               ##
 ##  date:    4/22/14                            ##
-##  version: 1.0                                ##
+##  version: 1.01                               ##
 ##################################################
 
 ## load modules                                        
@@ -225,12 +225,14 @@ def dressitup(D, Barcodes, sitemut, indel, locuslength, divis, addon, frags):
             use both chromosomes """
             if sitemut:
                 " remove loci if mutation arises in restriction site"
-                L1 = [list(D[samp][loc][0]) for copy in range(copies/2) if D[samp][loc][0][0:len(CUT1)+1] == D['4X0'][loc][0][0:len(CUT1)+1]] 
-                L2 = [list(D[samp][loc][1]) for copy in range(copies/2) if D[samp][loc][0][0:len(CUT1)+1] == D['4X0'][loc][0][0:len(CUT1)+1]] 
+                dropout = len(CUT+1)
+                L1 = [list(D[samp][loc][0]) for copy in range(copies/2) if D[samp][loc][0][0:dropout] == D['4X0'][loc][0][0:dropout]] 
+                L2 = [list(D[samp][loc][1]) for copy in range(copies/2) if D[samp][loc][0][0:dropout] == D['4X0'][loc][0][0:dropout]] 
                 if datatype not in ['rad']:
                     "if double digested (ddrad/gbs) check other end for locus dropout "
-                    L1 = [list(D[samp][loc][0]) for copy in range(copies/2) if D[samp][loc][0][-len(CUT2)-1:] == D['4X0'][loc][0][-len(CUT2)-1:]] 
-                    L2 = [list(D[samp][loc][1]) for copy in range(copies/2) if D[samp][loc][0][-len(CUT2)-1:] == D['4X0'][loc][0][-len(CUT2)-1:]]
+                    dropout2 = -len(CUT2)-1
+                    L1 = [list(D[samp][loc][0]) for copy in range(copies/2) if D[samp][loc][0][dropout2:] == D['4X0'][loc][0][dropout2:]] 
+                    L2 = [list(D[samp][loc][1]) for copy in range(copies/2) if D[samp][loc][0][dropout2:] == D['4X0'][loc][0][dropout2:]]
             else:
                 L1 = [list(D[samp][loc][0]) for copy in range(copies/2)] 
                 L2 = [list(D[samp][loc][1]) for copy in range(copies/2)]
